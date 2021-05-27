c42b3b0d77cfb450039415c4b255ab015cf50e39
Sunse7 <55135188+Sunse7@users.noreply.github.com>
2021-05-27 14:40:04 +0200

    Cleand up updateProfile API call

--- TinderWithATwist/TinderWithATwist/ClientApp/src/components/Utils/UpdateProfile.js
+++ TinderWithATwist/TinderWithATwist/ClientApp/src/components/Utils/UpdateProfile.js
@@ -1,35 +1,18 @@
-import imageToBase64 from 'image-to-base64';
 import authService from '../api-authorization/AuthorizeService';
-
-
-const toBase64 = file => new Promise((resolve, reject) => {
-    const reader = new FileReader();
-    reader.readAsDataURL(file);
-    reader.onload = () => {
-        const base64 = reader.result.replace('data:image/jpeg;base64,', ''); 
-        resolve(base64); 
-    }
-    reader.onerror = error => reject(error);
-});
-
+import { toBase64 } from './ToBase64';
 
 export const updateProfile = async (userProfilePicture) => {
     const [token, isMe, user, profilePicture] = await Promise.all([authService.getAccessToken(), authService.isAuthenticated(), authService.getUser(), toBase64(userProfilePicture)]);
-    //console.log('userProfile', userProfile);
      
-    //console.log('64pic', profilePicture);
     if (isMe) {
-      const response = await fetch(`ApplicationUser`, {
+      const response = await fetch(`ApplicationUser/${user.sub}`, {
         method: 'PUT',
         headers: !token ? {} : {
-          
-          'Content-Type' : 'application/json'
+          'Authorization': `Bearer ${token}`,
+          'Content-Type': 'application/json'
         },
         
-        body: JSON.stringify('hej')
+        body: JSON.stringify(profilePicture)
       });
-console.log('token', token);
-      //console.log('userProfile', userProfile);
-      console.log('response', response);
     }
   }
